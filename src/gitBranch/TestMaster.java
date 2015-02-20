75a6790ffb166ff63a49c7f32332dafb123682f5
mmjyothi <Ishansinghpatel@gmail.com>
2015-02-20 22:25:27 +0000

    Branch initial check in

---
+++ src/gitBranch/TestMaster.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package gitBranch;
+
+/**
+ * @author Jyothi
+ *
+ */
+public class TestMaster {
+
+	/**
+	 * 
+	 */
+	public TestMaster() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Version 0.0");
+		}
+
+}
